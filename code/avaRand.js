5493e7c5545b89976b130549d3c4332e99f561a0
VerloreN <nazilordgrief@gmail.com>
2020-02-27 08:21:48 +0300

    Small reDesign

--- code/avaRand.js
+++ code/avaRand.js
@@ -6,9 +6,9 @@
 const avatar = document.getElementById('ava');
 
 const photos = [
-    "https://sun9-40.userapi.com/c858224/v858224186/e9d0a/P9EFLU-vs_U.jpg", //0
-    "https://sun9-46.userapi.com/c855720/v855720295/11866f/IQmFioL-bV8.jpg", //1
-    "https://sun9-4.userapi.com/c854028/v854028423/15d5c/S95zSaQXyWM.jpg" //2
+    "./photos/1.png", //0
+    "./photos/2.png", //1
+    "./photos/3.png" //2
 ];
 
 let randNum = Math.floor(Math.random() * 3);
@@ -16,17 +16,17 @@ let randNum = Math.floor(Math.random() * 3);
 if (avatar) {
     switch (randNum) {
         case 0:
-            avatar.style.backgroundImage = `url(${photos[0]})`;
+            avatar.src = photos[0];
             console.log('Рандом аватарка: ', randNum);
         break;
 
         case 1:
-            avatar.style.backgroundImage = `url(${photos[1]})`;
+            avatar.src = photos[1];
             console.log('Рандом аватарка: ', randNum);
         break;
 
         case 2:
-            avatar.style.backgroundImage = `url(${photos[2]})`;
+            avatar.src = photos[2];
             console.log('Рандом аватарка: ', randNum);
         break;
     }
