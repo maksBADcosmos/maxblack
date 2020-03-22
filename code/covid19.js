667d8c304ecb8c4a36f41dd6d612b377e4e5cc30
VerloreN <nazilordgrief@gmail.com>
2020-03-22 05:05:45 +0300

    COVID-19 Special Update
    Added monitoring

---
+++ code/covid19.js
@@ -0,0 +1,57 @@
+/*
+    COVID-19 Monitoring
+    by Max Black.
+*/
+function numForm(number, decimals, dec_point, thousands_sep) {
+    number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
+    var n = !isFinite(+number) ? 0 : +number,
+        prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+        sep = (typeof thousands_sep === 'undefined') ? ' ' : thousands_sep,
+        dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+        s = '',
+        toFixedFix = function(n, prec) {
+            var k = Math.pow(10, prec);
+            return '' + (Math.round(n * k) / k)
+                .toFixed(prec);
+        };
+    // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+    s = (prec ? toFixedFix(n, prec) : '' + Math.round(n))
+        .split('.');
+
+    if (s[0].length > 3) {
+        s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+    }
+
+    if ((s[1] || '')
+        .length < prec) {
+        s[1] = s[1] || '';
+        s[1] += new Array(prec - s[1].length + 1)
+            .join('0');
+    }
+
+    return s.join(dec);
+}
+
+async function getData(url = '') {
+    const response = await fetch(url);
+    return await response.json();
+}
+
+getData('https://coronavirus-19-api.herokuapp.com/all')
+    .then((data) => {
+        if (data) {
+            if (document.getElementById('covid-infected') && document.getElementById('covid-recovered') && document.getElementById('covid-deaths')) {
+                document.getElementById('covid-infected').innerText = numForm(data.cases);
+                document.getElementById('covid-recovered').innerText = numForm(data.recovered);
+                document.getElementById('covid-deaths').innerText = numForm(data.deaths);
+            }
+
+            else {
+                console.log('Elements not found!');
+            }
+        }
+
+        else {
+            console.log('Unable to get data!');
+        }
+    });
