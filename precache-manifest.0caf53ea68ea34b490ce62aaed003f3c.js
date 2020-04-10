1959932e6c6623a91c6c9c1cd48d0a9f5183abff
VerloreN <37268930+maksBADcosmos@users.noreply.github.com>
2020-04-10 21:58:35 +0300

    Major UPDATE: New tech. stack (ReactJS migration)
    Time to upgrade!

---
+++ precache-manifest.0caf53ea68ea34b490ce62aaed003f3c.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0412c68e88159cbb41e29ecfbeaf43f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8c545393b4572e4586e",
+    "url": "/static/css/main.71493b48.chunk.css"
+  },
+  {
+    "revision": "a05c8e332224d9a303dc",
+    "url": "/static/js/2.6a90b5f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6a90b5f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8c545393b4572e4586e",
+    "url": "/static/js/main.bb54596d.chunk.js"
+  },
+  {
+    "revision": "6a6a9c912dcc8050fad6",
+    "url": "/static/js/runtime-main.3305178d.js"
+  },
+  {
+    "revision": "f4e2b25f57368aeb0b69ce759b53ee90",
+    "url": "/static/media/1.f4e2b25f.png"
+  },
+  {
+    "revision": "0fa44947c32c175ab46f74bafb27a102",
+    "url": "/static/media/2.0fa44947.png"
+  },
+  {
+    "revision": "38e41880694eeb86b0141010f6495426",
+    "url": "/static/media/3.38e41880.png"
+  },
+  {
+    "revision": "f220780e3dd8adda693707a5d0d3399f",
+    "url": "/static/media/coder.f220780e.png"
+  },
+  {
+    "revision": "3cd5f7c230aabbdc78195ca1737de6f4",
+    "url": "/static/media/cov.3cd5f7c2.png"
+  },
+  {
+    "revision": "63d31a5d331d3390c8e84ac2372fa784",
+    "url": "/static/media/dark_coder.63d31a5d.png"
+  },
+  {
+    "revision": "c40d16f9854a897121103ef2382d62de",
+    "url": "/static/media/design.c40d16f9.png"
+  },
+  {
+    "revision": "3c0d84721495154a6fca6113e17d3be3",
+    "url": "/static/media/frowning_face.3c0d8472.png"
+  },
+  {
+    "revision": "a3d9c56147e346bc69baeee122b50740",
+    "url": "/static/media/history.a3d9c561.png"
+  },
+  {
+    "revision": "9e39c403a8d5cfa14dd903509c8577a6",
+    "url": "/static/media/insta.9e39c403.png"
+  },
+  {
+    "revision": "92d586ef5802abba6947786acb31486c",
+    "url": "/static/media/map.92d586ef.png"
+  },
+  {
+    "revision": "ab05d180883373e0528d57becbad6883",
+    "url": "/static/media/music.ab05d180.png"
+  },
+  {
+    "revision": "fa856f5e4675f9e54be341996194ebd4",
+    "url": "/static/media/news.fa856f5e.svg"
+  },
+  {
+    "revision": "b5da610ae45398e807313d02bb59e097",
+    "url": "/static/media/patreon.b5da610a.png"
+  },
+  {
+    "revision": "63d377e5daeccb307dd5d894f421e3d1",
+    "url": "/static/media/phone.63d377e5.svg"
+  },
+  {
+    "revision": "56d0ef5200101fdc3c210d094b993cc4",
+    "url": "/static/media/recom.56d0ef52.svg"
+  },
+  {
+    "revision": "6c6cd437fe337f98b14d5d2f5f238932",
+    "url": "/static/media/sites.6c6cd437.svg"
+  },
+  {
+    "revision": "310e66470987e71cecf7d607fe529bfd",
+    "url": "/static/media/skull.310e6647.png"
+  },
+  {
+    "revision": "2caaf9b209494aaf5e156c5f6fe77194",
+    "url": "/static/media/sneezing_face.2caaf9b2.png"
+  },
+  {
+    "revision": "166f07b68e70f7c95c072c7c60d6ba3d",
+    "url": "/static/media/steam.166f07b6.png"
+  },
+  {
+    "revision": "7b8caa18aa21ace8195d872409fb7d03",
+    "url": "/static/media/tg.7b8caa18.png"
+  },
+  {
+    "revision": "64f100f1f3990c1f1584f56574c43edc",
+    "url": "/static/media/tools.64f100f1.png"
+  },
+  {
+    "revision": "2cb961046664f6879848b2dfbcec5226",
+    "url": "/static/media/tumblr.2cb96104.png"
+  },
+  {
+    "revision": "dc2a59ce7ad1599ac04b53aacd655959",
+    "url": "/static/media/twitter.dc2a59ce.png"
+  },
+  {
+    "revision": "07cc44cb528eb071bcde0fef46beab4f",
+    "url": "/static/media/vk.07cc44cb.png"
+  },
+  {
+    "revision": "ee716bd477b853fb5312532971af57a8",
+    "url": "/static/media/winking_f.ee716bd4.png"
+  }
+]);
